a8b013be11aad8497c334117f1fbcdc6354cea94
James Harvey <44349936+jmshrv@users.noreply.github.com>
2025-11-29 19:06:01 +0000

    Initial Commit

---
+++ OptimalPint/OptimalPintApp.swift
@@ -0,0 +1,17 @@
+//
+//  OptimalPintApp.swift
+//  OptimalPint
+//
+//  Created by James Harvey on 29/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct OptimalPintApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
