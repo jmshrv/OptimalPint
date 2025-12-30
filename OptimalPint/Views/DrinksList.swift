7b6d9ad17c4d7e898a999437973a80f5cb0e76ee
James Harvey <44349936+jmshrv@users.noreply.github.com>
2025-12-30 15:04:27 +0000

    Make drinks searchable

--- OptimalPint/Views/DrinksList.swift
+++ OptimalPint/Views/DrinksList.swift
@@ -9,35 +9,57 @@ import SwiftUI
 
 struct DrinksList: View {
     private let drinks: [Drink]
-    
+
+    @State private var searchTerm = ""
+
     init(drinks: [Drink]) {
         self.drinks = drinks.sorted { $0.optimality < $1.optimality }
     }
-    
+
     var body: some View {
-        List(drinks) { drink in
+        List(listContent) { drink in
             HStack {
                 VStack(alignment: .leading) {
                     Text(drink.name)
                     Text("\(drink.units.formatted()) units")
                         .foregroundStyle(.secondary)
-                    
+
                     if let dealDescription = drink.dealDescription {
                         Text(dealDescription)
                             .foregroundStyle(.tertiary)
                     }
                 }
-                
+
                 Spacer()
-                
+
                 VStack(alignment: .trailing) {
-                    Text("\(drink.optimality.formatted(.currency(code: "GBP")))/unit")
-                        .bold()
+                    Text(
+                        "\(drink.optimality.formatted(.currency(code: "GBP")))/unit"
+                    )
+                    .bold()
                     Text("\(drink.price.formatted(.currency(code: "GBP")))")
                         .foregroundStyle(.secondary)
                 }
             }
         }
+        .searchable(text: $searchTerm)
+    }
+
+    private var listContent: [Drink] {
+        if searchTerm.isEmpty {
+            return drinks
+        }
+
+        let trimmedSearchTerm = searchTerm.trimmingCharacters(
+            in: .whitespacesAndNewlines
+        )
+
+        return
+            drinks
+            .filter {
+                $0.name.localizedStandardContains(trimmedSearchTerm)
+            }
+            .sorted { $0.name < $1.name }
     }
 }
 
